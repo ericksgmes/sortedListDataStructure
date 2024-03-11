78302164bfe06bdd4b142f9e996da1204f24b3e3
Erick Santinon Gomes <erick.dicksan@gmail.com>
2024-03-11 13:37:39 -0300

    feat(createList): add createList method

--- main.c
+++ main.c
@@ -1,6 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+typedef struct {
+    int *array;
+    int maxSize;
+    int length;
+} t_sortedList;
+
+t_sortedList * createList(int size) {
+    t_sortedList *list = (t_sortedList *) malloc(sizeof(t_sortedList));
+    // ponteiro stack -> struct na heap
+    list->maxSize = size;
+    list->length = 0;
+    list->array = (int *) calloc(sizeof(int), list->maxSize);
+    return list;
+}
+
 int main(int argc, char *argv[]) {
 
     return 0;
